c11f6cbd0c158ff3d127d3ec27f50f315c86afac
Max534983 <534983@student.saxion.nl>
2022-11-30 11:41:50 +0100

    a
    b

---
+++ MonopolyProject/Assets/Scripts/Dobbelsteen.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dobbelsteen : MonoBehaviour
+{
+    int dice1 = 0;
+    int dice2 = 0;
+    int totalOnDice = 0;
+    bool dubbel = false;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        throwDice();
+    }
+
+    public void throwDice()
+    {
+        dice1 = 0;
+        dice2 = 0;
+        totalOnDice = 0;
+        dubbel = false;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dice1 = Random.Range(1, 7);
+            dice2 = Random.Range(1, 7);
+
+            if (dice1 == dice2)
+            {
+                dubbel = true;
+            }
+
+            totalOnDice = dice1 + dice2;
+            Debug.Log("---------------------------------------------------------------");
+            Debug.Log("Dice 1 was: " + dice1);
+            Debug.Log("Dice 2 was: " + dice2);
+            Debug.Log("Total on dice is: " + totalOnDice);
+            Debug.Log("Is it a dubbel: " + dubbel);
+        }
+    }
+}
