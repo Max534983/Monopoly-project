9a8337c81c3bbf5d0baeb56560b8bb1668a3ad84
Max534983 <534983@student.saxion.nl>
2022-12-13 16:58:35 +0100

    De dobbolsteen logica werkt nu

--- MonopolyProject/Assets/Scripts/Game_controller.cs
+++ MonopolyProject/Assets/Scripts/Game_controller.cs
@@ -17,12 +17,12 @@ public class Game_controller : MonoBehaviour
     public Text textPlayer_4;
     public Text textPlayer_5;
     public Text textPlayer_6;
-    public Text textThrew_1;
-    public Text textThrew_2;
-    public Text textThrew_3;
-    public Text textThrew_4;
-    public Text textThrew_5;
-    public Text textThrew_6;
+    public Text textLower_1;
+    public Text textLower_2;
+    public Text textLower_3;
+    public Text textLower_4;
+    public Text textLower_5;
+    public Text textLower_6;
     public Text ErrorBox;
 
     public Animator dobbolsteen1_animation;
@@ -31,6 +31,7 @@ public class Game_controller : MonoBehaviour
     int huidige_speler = 0;
 
     bool firstThrow = true;
+    bool diceThrown = false;
     int dice1 = 0;
     int dice2 = 0;
     int totalOnDice = 0;
@@ -53,9 +54,10 @@ public class Game_controller : MonoBehaviour
 
     public void playerTurn() 
     {
-        if (firstThrowValue[huidige_speler] != 0)
+        if (firstThrowValue[huidige_speler] != 0 || (firstThrow == false && diceThrown == true && dubbel == false))
         {
             huidige_speler++;
+            diceThrown = false;
         }
         else
         {
@@ -138,83 +140,91 @@ public class Game_controller : MonoBehaviour
         }
     }
 
+    //De logica voor een dobbolsteen gooien en firstThrow
     public void throwDice()
     {
-        dice1 = 0;
-        dice2 = 0;
-        totalOnDice = 0;
-        dubbel = false;
-        dubbel1 = false;
-
-        if (currentError.Equals("throwDice"))
+        if (diceThrown == false || firstThrow)
         {
-            error("noError");
-            currentError = "noError";
-        }
+            dice1 = 0;
+            dice2 = 0;
+            totalOnDice = 0;
+            dubbel = false;
+            dubbel1 = false;
 
-        dice1 = UnityEngine.Random.Range(1, 7);
-        dice2 = UnityEngine.Random.Range(1, 7);
+            diceThrown = true;
 
-        if (dice1 == dice2)
-        {
+            if (currentError.Equals("throwDice"))
+            {
+                error("noError");
+                currentError = "noError";
+            }
+
+            dice1 = UnityEngine.Random.Range(1, 7);
+            dice2 = UnityEngine.Random.Range(1, 7);
+
+            if (dice1 == dice2)
+            {
                 dubbel = true;
-        }
+                diceThrown = false;
+            }
 
-        if (dice1 == 1 && dice2 == 1)
-        {
-            dubbel1 = true;
-        }
+            if (dice1 == 1 && dice2 == 1)
+            {
+                dubbel1 = true;
+            }
 
-        totalOnDice = dice1 + dice2;
+            totalOnDice = dice1 + dice2;
 
-        if (firstThrow == true)
-        {
+            if (firstThrow == true)
+            {
                 firstThrowValue[huidige_speler] = totalOnDice;
+                showFirstNumberThrown();
+                playerTurn();
+            }
+
+            if (firstThrowValue[firstThrowValue.Length - 1] > 0 && firstThrow == true)
+            {
+                firstThrow = false;
+                endFirstThrow();
+                Debug.Log("first throw is klaar");
+            }
+
+            dobbolsteen1_animation.SetInteger("Dice", dice1);
+            dobbolsteen2_animation.SetInteger("Dice", dice2);
         }
-
-        if (firstThrowValue[firstThrowValue.Length - 1] > 0)
-        {
-            firstThrow = false;
-        }
-
-        dobbolsteen1_animation.SetInteger("Dice", dice1);
-        dobbolsteen2_animation.SetInteger("Dice", dice2);
-
-        Debug.Log("Total on dice is: " + totalOnDice);
-        showNumberThrown();
     }
 
-    public void showNumberThrown()
+    public void showFirstNumberThrown()
     {
         if (huidige_speler == 0)
         {
-            textThrew_1.text = "Threw: " + totalOnDice;
+            textLower_1.text = "Threw: " + totalOnDice;
         }
         else if (huidige_speler == 1)
         {
-            textThrew_2.text = "Threw: " + totalOnDice;
+            textLower_2.text = "Threw: " + totalOnDice;
         }
         else if (huidige_speler == 2)
         {
-            textThrew_3.text = "Threw: " + totalOnDice;
+            textLower_3.text = "Threw: " + totalOnDice;
         }
         else if (huidige_speler == 3)
         {
-            textThrew_4.text = "Threw: " + totalOnDice;
+            textLower_4.text = "Threw: " + totalOnDice;
         }
         else if (huidige_speler == 4)
         {
-            textThrew_5.text = "Threw: " + totalOnDice;
+            textLower_5.text = "Threw: " + totalOnDice;
         }
         else if (huidige_speler == 5)
         {
-            textThrew_6.text = "Threw: " + totalOnDice;
+            textLower_6.text = "Threw: " + totalOnDice;
         }
     }
 
-
+    //hier staan alle soorten error's die op het scherm mogen verschijnen
     public void error(string errorType)
-    {
+    {   
         if (errorType.Equals("throwDice"))
         {
             ErrorBox.gameObject.SetActive(true);
@@ -227,27 +237,72 @@ public class Game_controller : MonoBehaviour
         }
     }
 
-    public void gameStartUi() 
+    public void endFirstThrow() 
     {
+        string[] playerNamesUpdate = new string[] { "", "", "", "", "", "" };
+
+        //Kijk wie er het hoogste gooide
+        for (int i = 0; i < playerNames.Length; i++)
+        {
+            int spelerMetHoogsteNummer = 0;
+            int hoogesteNummer = 0;
+            for (int y = 0; y < playerNames.Length; y++) 
+            {
+                if (firstThrowValue[y] > hoogesteNummer)
+                {
+                    spelerMetHoogsteNummer = y;
+                    hoogesteNummer = firstThrowValue[y];
+                }
+            }
+            firstThrowValue[spelerMetHoogsteNummer] = 0;
+            playerNamesUpdate[i] = playerNames[spelerMetHoogsteNummer];
+            Debug.Log(playerNamesUpdate[i]);
+        }
+
+        //verander de namen. laat de mensen die het hoogst gooien op volgorde gaan.
+        for (int i = 0; i < playerNames.Length; i++)
+        {
+            playerNames[i] = playerNamesUpdate[i];
+        }
+
+        diceThrown = false;
+        gameStartUi();
+        updateMoney();
+    }
+
+    //Zorg dat de UI er goed uitziet wanneer het spel start
+    public void gameStartUi() 
+    {   
         textPlayer_1.text = playerNames[0];
-        textPlayer_1.color = Color.green;
+        textPlayer_1.color = Color.red;
 
         textPlayer_2.text = playerNames[1];
-        textPlayer_2.color = Color.red;
+        textPlayer_2.color = Color.green;
 
         textPlayer_3.text = playerNames[2];
-        textPlayer_3.color = Color.red;
+        textPlayer_3.color = Color.green;
 
         textPlayer_4.text = playerNames[3];
-        textPlayer_4.color = Color.red;
+        textPlayer_4.color = Color.green;
 
         textPlayer_5.text = playerNames[4];
-        textPlayer_5.color = Color.red;
+        textPlayer_5.color = Color.green;
 
         textPlayer_6.text = playerNames[5];
-        textPlayer_6.color = Color.red;
+        textPlayer_6.color = Color.green;
 
         ErrorBox.color = Color.red;
         ErrorBox.gameObject.SetActive(false);
+    }
+
+    public void updateMoney() 
+    {
+        //TODO zorg dat het geld dat spelers hebben op het scherm zichtbaar wordt.
+        textLower_1.enabled = false;
+        textLower_2.enabled = false;
+        textLower_3.enabled = false;
+        textLower_4.enabled = false;
+        textLower_5.enabled = false;
+        textLower_6.enabled = false;
     }
 }
