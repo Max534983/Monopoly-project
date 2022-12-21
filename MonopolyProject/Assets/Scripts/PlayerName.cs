321b5c7c84e2ae695bc9d2fdf750e0eae62be947
Mika535689 <535689@student.saxion.nl>
2022-12-21 09:43:16 +0100

    Player namen worden gesaved

---
+++ MonopolyProject/Assets/Scripts/PlayerName.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerNames : MonoBehaviour
+{
+    public static string Player1;
+    public static string Player2;
+    public static string Player3;
+    public static string Player4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- MonopolyProject/Assets/Scripts/ReadInput.cs
+++ MonopolyProject/Assets/Scripts/ReadInput.cs
@@ -8,4 +8,27 @@ public class ReadInput : MonoBehaviour
 
     private string input;
 
+    public void ReadStringInput1(string player1)
+    {
+    input = player1;
+    PlayerNames.Player1 = input;
+    }
+
+    public void ReadStringInput2(string player2)
+    {
+    input = player2;
+    PlayerNames.Player2 = input;
+    }
+
+    public void ReadStringInput3(string player3)
+    {
+    input = player3;
+    PlayerNames.Player3 = input;
+    }
+
+    public void ReadStringInput4(string player4)
+    {
+    input = player4;
+    PlayerNames.Player4 = input;
+    }
 }
