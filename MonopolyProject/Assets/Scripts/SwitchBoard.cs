0f64915e4bbd27fc8fc9b9be951a9aa40bf67c08
Mika535689 <535689@student.saxion.nl>
2023-01-23 20:29:41 +0100

    revert

--- MonopolyProject/Assets/Scripts/SwitchBoard.cs
+++ MonopolyProject/Assets/Scripts/SwitchBoard.cs
@@ -1,23 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
-
 
 public class SwitchBoard : MonoBehaviour
 {
-public GameObject GameObjectToDeactivate;
-
 public void playerCount()
 {
 if (PlayerNames.Player1 != null && PlayerNames.Player2 != null && PlayerNames.Player3 != null && PlayerNames.Player4 != null){
 SceneManager.LoadScene(0);
 }
+<<<<<<< HEAD
 else {
 SceneManager.LoadScene(2);
 PlayerNames.Player1 = null;
@@ -27,12 +20,24 @@ PlayerNames.Player4 = null;
 SceneManager.LoadScene(1);
 GameObjectToDeactivate.SetActive(!GameObjectToDeactivate.activeInHierarchy);
 }
+=======
+else SceneManager.LoadScene(2);
+>>>>>>> parent of d10723c (bord veranderd)
 }
+[SerializeField] private string board = "Board";
     public void CloseButton()
     {
+<<<<<<< HEAD
         GameObjectToDeactivate.SetActive(false);
+=======
+        SceneManager.LoadScene(2);
+        PlayerNames.Player1 = null;
+        PlayerNames.Player2 = null;
+        PlayerNames.Player3 = null;
+        PlayerNames.Player4 = null;
+        SceneManager.LoadScene(board);
+>>>>>>> parent of d10723c (bord veranderd)
     }
-
 
 
 }
