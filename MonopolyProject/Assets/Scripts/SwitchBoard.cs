d10723cdee31890905957e8f073c9e7436faefb9
Mika535689 <535689@student.saxion.nl>
2023-01-23 16:59:20 +0100

    bord veranderd

--- MonopolyProject/Assets/Scripts/SwitchBoard.cs
+++ MonopolyProject/Assets/Scripts/SwitchBoard.cs
@@ -1,27 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+
 
 public class SwitchBoard : MonoBehaviour
 {
+public GameObject GameObjectToDeactivate;
+
 public void playerCount()
 {
 if (PlayerNames.Player1 != null && PlayerNames.Player2 != null && PlayerNames.Player3 != null && PlayerNames.Player4 != null){
 SceneManager.LoadScene(0);
 }
-else SceneManager.LoadScene(2);
+else {
+SceneManager.LoadScene(2);
+PlayerNames.Player1 = null;
+PlayerNames.Player2 = null;
+PlayerNames.Player3 = null;
+PlayerNames.Player4 = null;
+SceneManager.LoadScene(1);
+GameObjectToDeactivate.SetActive(true);
 }
-[SerializeField] private string board = "Board";
+}
     public void CloseButton()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(0);
         PlayerNames.Player1 = null;
         PlayerNames.Player2 = null;
         PlayerNames.Player3 = null;
         PlayerNames.Player4 = null;
-        SceneManager.LoadScene(board);
+        SceneManager.LoadScene(1);
+        GameObjectToDeactivate.SetActive(false);
     }
+
 
 
 }
